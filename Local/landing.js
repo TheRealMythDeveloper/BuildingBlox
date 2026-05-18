f3d83869b945593397617a7a55bd1916b6953ccc
TheRealMythDeveloper <therealmythdev@gmail.com>
2026-05-18 21:31:17 +0000

    add some pages

---
+++ Local/landing.js
@@ -0,0 +1,20 @@
+function enterSite() {
+  document.body.classList.add("fade-out");
+
+  setTimeout(() => {
+    window.location.href = "./index.html";
+  }, 800);
+}
+document.addEventListener("mousemove", (e) => {
+  const x = (e.clientX / window.innerWidth - 0.5) * 20;
+  const y = (e.clientY / window.innerHeight - 0.5) * 20;
+
+  document.querySelector(".container").style.transform =
+    `translate(-50%, -50%) translate(${x}px, ${y}px)`;
+});
+const cursor = document.getElementById("cursor");
+
+document.addEventListener("mousemove", (e) => {
+  cursor.style.left = e.clientX + "px";
+  cursor.style.top = e.clientY + "px";
+});
